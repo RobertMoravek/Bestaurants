e623b916df73d11720d922cf042a2dc12965a3b5
Robert Morávek <robert.moravek@gmail.com>
2022-08-29 15:23:12 +0200

    gitignore

--- server/index.js
+++ server/index.js
@@ -7,48 +7,60 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
 
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
 
 app.get("/api", async (req, res) => {
-    
     res.json({ message: "API!" });
-    
 });
 
 app.get("/searchoptionscountries", async (req, res) => {
     let countriesNames = fs.readdirSync(__dirname + "/../restaurants/");
     res.json(countriesNames);
-    
 });
 
 app.get("/searchoptionscities/:country", async (req, res) => {
     console.log(req.params.country);
-    let citiesNames = fs.readdirSync(__dirname + "/../restaurants/" + req.params.country);
+    let citiesNames = fs.readdirSync(
+        __dirname + "/../restaurants/" + req.params.country
+    );
     res.json(citiesNames);
-    
 });
 
 app.get("/searchoptionsrestaurants/:country/:city", async (req, res) => {
     console.log(req.params);
-    let restaurantTypes = fs.readdirSync(__dirname + "/../restaurants/" + req.params.country + "/" + req.params.city);
-    restaurantTypes = restaurantTypes.map(item => item.slice(0, -5))
+    let restaurantTypes = fs.readdirSync(
+        __dirname +
+            "/../restaurants/" +
+            req.params.country +
+            "/" +
+            req.params.city
+    );
+    restaurantTypes = restaurantTypes.map((item) => item.slice(0, -5));
     res.json(restaurantTypes);
-    
 });
 
 app.get("/searchoptionsresults/:country/:city/:type", async (req, res) => {
     console.log(req.params);
-    let results = (fs.readFileSync(path.join(__dirname + "/../restaurants/" + req.params.country + "/" + req.params.city + "/" + req.params.type + ".json"))).toString();
+    let results = fs
+        .readFileSync(
+            path.join(
+                __dirname +
+                    "/../restaurants/" +
+                    req.params.country +
+                    "/" +
+                    req.params.city +
+                    "/" +
+                    req.params.type +
+                    ".json"
+            )
+        )
+        .toString();
     // console.log(results);
     res.json(results);
-    
 });
 
-
-
-
-app.listen(process.env.PORT ||PORT, () => {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
