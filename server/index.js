8c79186c32c74acb823ace4d9a267aff18d2b990
Robert Morávek <robert.moravek@gmail.com>
2022-08-29 15:20:03 +0200

    gitignore

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+
 app.use(express.static(path.join(__dirname, "..", "client", "public")));
 
 app.get("/api", async (req, res) => {
